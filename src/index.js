68c55f33eb38a98ea7554bb0b080b683e0334c07
piyushpal123 <piyushpal9536@gmail.com>
2026-02-21 17:42:06 +0530

    utils setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import ConnectDb from "./db/index.js";
 
+import "dotenv/config";
+
+const port = process.env.PORT||8000;
 
 
+ConnectDb()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`Server is running at ${port}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB is FAILED ",err);
 
-ConnectDb();
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+
+
+const aysncHandler = (requestHandler)=>(req,res,next)=>{
+    Promise().resolve(requestHandler(req,res,next)).catch((err)=>next(err));
+
+}
+
+export {asyncHandler}
