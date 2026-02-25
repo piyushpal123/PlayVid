35e2feca3c3e14bf4092547351b1a45af421481d
piyushpal123 <piyushpal9536@gmail.com>
2026-02-25 19:37:57 +0530

    Cloudinary setup

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -4,7 +4,7 @@ class ApiError extends Error{
         statusCode,
         message = "Something went Wrong",
         error = [],
-        statck = ""
+        stack = ""
 
     ){
         super(message)
@@ -14,8 +14,8 @@ class ApiError extends Error{
         this.success = false,
         this.errors = error
 
-        if(statck){
-            this.stack = statck;
+        if(stack){
+            this.stack = stack;
         }
         else{
             Error.captureStackTrace(this,this.constructor)
---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+    const uploadOnCLoudinary = async (localfilepath)=>{
+
+        if(!localfilepath)return null;
+        try {
+          const response =  await cloudinary.uploader.upload(localfilepath,{
+            resource_type:"auto"
+           })
+
+           //file successful upload 
+           console.log("file is uploaded ",response.url);
+           return response;
+
+           
+            
+        } catch (error) {
+        fs.unlinkSync(localfilepath)
+          
+       
+            
+        
+
+    }
+}
+})
