47341e4307411cb86124b142fa5255147d79de2b
piyushpal123 <piyushpal9536@gmail.com>
2026-03-21 00:45:54 +0530

    feat: add healthcheck route and implement channel stats retrieval; update video model references

--- src/app.js
+++ src/app.js
@@ -17,8 +17,26 @@ app.use(cookieParser())
 
 // import routes 
 import userRouter from './routes/user.routes.js';
+import VedioRouter from './routes/vedio.routes.js'
+import commentRouter from './routes/comment.routes.js'
+import LikeRouter from './routes/like.routes.js'
+import tweetRouter from './routes/tweet.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
+import subcriptionRouter from './routes/subcription.routes.js'
+import dashboardRouter from './routes/dashboard.routes.js'
+import healthcheckRouter from './routes/healthcheck.routes.js'
 
 app.use("/api/v1/users",userRouter);
+app.use("/api/v1/vedio",VedioRouter);
+app.use("/api/v1/comment",commentRouter);
+app.use("/api/v1/Like",LikeRouter);
+app.use("/api/v1/playlist",playlistRouter);
+app.use("/api/v1/subcription",subcriptionRouter);
+app.use("/api/v1/dashboardRouter",dashboardRouter);
+app.use("/api/v1/healthcheck",healthcheckRouter);
+app.use("/api/v1/tweet",tweetRouter);
+
+
 
 // http://localhost:8000/api/v1/users/register
 
